3192500eeb4bde486f8e5eef304488d5daf320c6
Mireya-Hernandez <42775432+Mireya-Hernandez@users.noreply.github.com>
2020-12-07 03:45:07 -0500

    Added the Observer Design Pattern

--- src/sample/Message.java
+++ src/sample/Message.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class Message {
-  final String messageContent;
+  final String messageContent;//test
 
   public Message(String message) {
     this.messageContent = message;
